748050c6f06919c17fc27e055fbb897fa272a47c
Elena <MissKatharsis@yandex.ru>
2023-01-15 20:03:58 +0300

    refactoring code and separate this on 3 modules

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,10 +1,8 @@
-import readlineSync from 'readline-sync';
+import getRandomInRange from '../getRandomNumbers.js';
+import runGame from '../index.js'
 
 const descriptionGame = 'What is the result of the expression? ';
-
-const getRandomInRange = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+const operators = ['+', '-', '*'];
 
 const calculate = (num1, operator, num2) => {
     switch (operator) {
@@ -19,36 +17,15 @@ const calculate = (num1, operator, num2) => {
     }
 };
 
-const playGame = () => {
-    const operations = ['+', '-', '*'];
+const getRoundsGame = () => {
     const num1 = getRandomInRange(1, 20);
     const num2 = getRandomInRange(1, 20);
-    const operator = operations[getRandomInRange(0, (operations.length - 1))];
+    const operator = operators[getRandomInRange(0, (operators.length - 1))];
     const question = `${num1} ${operator} ${num2}`;
     const correctAnswer = String(calculate(num1, operator, num2));
     return [question, correctAnswer];
 };
 
-const runGame = (descriptionGame, getRandomInt) => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-    console.log(descriptionGame);
+const startGame = () => runGame(descriptionGame, getRoundsGame);
 
-const startGame = () => gameRounds(descriptionGame, playGame);
-startGame();
-
-    const roundsCount = 3;
-    for (let i = 0; i < roundsCount; i += 1) {
-        const [question, correctAnswer] = getRandomInt();
-        console.log(`Question: ${question}`);
-        const answer = readlineSync.question('Your answer: ');
-        if (answer !== correctAnswer) {
-            console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-            console.log(`Let's try again, ${userName}!`);
-            return;
-        } console.log('Correct!');
-    } console.log(`Congratulations, ${userName}!`);
-};
-
-export default runGame;
+export default startGame;
