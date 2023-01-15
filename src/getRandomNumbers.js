a7afeb96fc0d4b9fb496258df1e40205a34ad24e
Elena <MissKatharsis@yandex.ru>
2023-01-15 19:19:21 +0300

    add 2 modules

---
+++ src/getRandomNumbers.js
@@ -0,0 +1,5 @@
+const getRandomInRange = (min = 1, max = 100) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export default getRandomInRange;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+const levelCount = 3;
+
+const runGame = (descriptionGame, getRoundsGame) => {
+    console.log('Welcome to the Brain Games!');
+    const playerName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${playerName}!`);
+    console.log(descriptionGame);
+
+    for (let i = 0; i < levelCount; i += 1) {
+        const [question, correctAnswer] = getRoundsGame();
+        console.log(`Question: ${question}`);
+        const playerAnswer = readlineSync.question('Your answer: ');
+        if (playerAnswer !== correctAnswer) {
+            console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+            console.log(`Let's try again, ${playerName}!`);
+            return;
+        }
+
+        console.log('Correct!');
+    }
+
+    console.log(`Congratulations, ${playerName}!`);
+};
+
+export default runGame;
