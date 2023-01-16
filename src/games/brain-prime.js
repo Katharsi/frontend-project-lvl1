2bf143560cd18cb0ecd80612f9a41eb389cf854a
Elena <MissKatharsis@yandex.ru>
2023-01-16 13:06:02 +0300

    refactoring code for brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,16 +1,15 @@
-import readlineSync from 'readline-sync';
+import getRandomInRange from '../getRandomNumbers.js';
+import runGame from '../index.js'
 
 const descriptionGame = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-const getRandomInRange = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
 
 const isPrime = (number) => {
     if (number < 2) {
       return false;
     }
+
     let i = 2;
+
     while (i < number / 2) {
       if (number % i === 0) {
         return false;
@@ -20,30 +19,13 @@ const isPrime = (number) => {
     return true;
 };
 
-export const getRandomInt = () => {
-    const randomNumber = getRandomInRange(1, 30);
-    const correctAnswer = isEven(randomNumber) ? "yes" : "no";
-    return [randomNumber, correctAnswer];
+const getRoundsGame = () => {
+    const question = getRandomInRange(2, 100);
+    const correctAnswer = isPrime(question) ? "yes" : "no";
+
+    return [question, correctAnswer];
 };
 
-export const runGame = (descriptionGame, getRandomInt) => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-    console.log(descriptionGame);
-    const roundsCount = 3;
-    for (let i = 0; i < roundsCount; i += 1) {
-        const [question, correctAnswer] = getRandomInt();
-        console.log(`Question: ${question}`);
-        const answer = readlineSync.question('Your answer: ');
-        if (answer !== correctAnswer) {
-            console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
-            console.log(`Let's try again, ${userName}!`);
-            return;
-        } console.log('Correct!');
-    } console.log(`Congratulations, ${userName}!`);
-};
-
-const startGame = () => gameRounds(descriptionGame, getRandomInt);
+const startGame = () => runGame(descriptionGame, getRoundsGame);
 
 export default startGame;
